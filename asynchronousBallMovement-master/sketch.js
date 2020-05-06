d61dd4a45bfebc322f6e633dd4d460ebc04f5409
Ayush Bhardwaj <62342389+AB270@users.noreply.github.com>
2020-05-06 14:05:09 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,32 @@
+var database;
+
+var canvas,form,player,game;
+var allPlayers;
+var distance=0;
+
+var gameState=0;
+var playerCount;
+
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    game=new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    if(playerCount===4){
+      game.update(1);
+    }
+
+    if(gameState===1){
+        clear();
+        game.play();
+      }
+
+
+}
+
